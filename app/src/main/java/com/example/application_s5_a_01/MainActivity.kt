18e8c010f2d66a1e6a5548410f710cf76934e021
Adrien Chenu <Adrien.Chenu@etu.univ-smb.fr>
2023-12-20 09:47:41 +0100

    Add dropdown menu

--- app/src/main/java/com/example/application_s5_a_01/MainActivity.kt
+++ app/src/main/java/com/example/application_s5_a_01/MainActivity.kt
@@ -1,8 +1,11 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.application_s5_a_01
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,21 +14,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import co.yml.charts.axis.AxisData
-import co.yml.charts.common.model.AccessibilityConfig
 import co.yml.charts.common.model.Point
-import co.yml.charts.ui.barchart.BarChart
-import co.yml.charts.ui.barchart.models.BarChartData
-import co.yml.charts.ui.barchart.models.BarChartType
-import co.yml.charts.ui.barchart.models.BarData
-import co.yml.charts.ui.barchart.models.BarStyle
-import co.yml.charts.ui.barchart.models.drawBarGraph
 import co.yml.charts.ui.linechart.LineChart
 import co.yml.charts.ui.linechart.model.GridLines
 import co.yml.charts.ui.linechart.model.IntersectionPoint
@@ -38,6 +31,17 @@ import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
 import co.yml.charts.ui.linechart.model.ShadowUnderLine
 import com.example.application_s5_a_01.ui.theme.Application_S5_A_01Theme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,9 +52,67 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(name = "Axel")
-                    BarChart()
+                    //Greeting(name = "Axel")
+                    //BarChart()
+                    SimpleDropdownMenu()
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun SimpleDropdownMenu() {
+    var isExpended by remember {
+        mutableStateOf(false)
+    }
+
+    var gender by remember {
+        mutableStateOf("")
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = isExpended,
+            onExpandedChange = { isExpended = it}
+        ) {
+            TextField(
+                value = gender,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpended)
+                },
+                colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                modifier = Modifier.menuAnchor()
+            )
+
+            ExposedDropdownMenu(
+                expanded = isExpended,
+                onDismissRequest = { isExpended = false }
+            ) {
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "Male")
+                    },
+                    onClick = {
+                        gender = "Male"
+                        isExpended = false
+                    }
+                )
+
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "Female")
+                    },
+                    onClick = {
+                        gender = "Female"
+                        isExpended = false
+                    }
+                )
             }
         }
     }
