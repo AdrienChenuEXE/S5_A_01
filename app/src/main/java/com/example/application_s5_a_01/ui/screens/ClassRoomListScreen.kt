1acfe59d2ae26be382c8ef596e0c136bfe4017b7
a.ledevedec <arthur.ledevedec@webexpertbusiness.net>
2024-03-11 16:05:30 +0100

    bottombar & classroomlist

--- app/src/main/java/com/example/application_s5_a_01/ui/screens/ClassRoomListScreen.kt
+++ app/src/main/java/com/example/application_s5_a_01/ui/screens/ClassRoomListScreen.kt
@@ -1,17 +1,31 @@
 package com.example.application_s5_a_01.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,10 +35,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.application_s5_a_01.R
 import com.example.application_s5_a_01.model.ClassRoom
+import com.example.application_s5_a_01.utils.GraphicUtils
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextFieldDefaults
+import com.example.application_s5_a_01.ui.BottomMenu
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,35 +63,82 @@ fun ClassRoomListScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Class Rooms") },
+                title = { Text("Bienvenue") }, // Ajout du titre "Bienvenue"
                 actions = {
-                    TextField(
-                        value = searchText.value,
-                        onValueChange = { searchText.value = it },
-                        placeholder = { Text("Search classrooms") },
-                        singleLine = true
-                    )
+                    Box(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = searchText.value,
+                            onValueChange = { searchText.value = it },
+                            placeholder = { Text("Search classrooms") },
+                            singleLine = true,
+                            modifier = Modifier.fillMaxWidth(),
+                            leadingIcon = {
+                                Icon(Icons.Default.Search, contentDescription = "Search")
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                cursorColor = MaterialTheme.colorScheme.onSurface,
+                                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                                unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
+                            ),
+                            textStyle = MaterialTheme.typography.bodyMedium,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                    }
                 }
             )
         },
-        content = { innerPadding ->
-            LazyColumn(
-                modifier = Modifier.padding(innerPadding),
-                verticalArrangement = Arrangement.spacedBy(10.dp)
+        content = {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
-                items(classRooms.filter {
-                    it.name.contains(
-                        searchText.value.text,
-                        true
-                    )
-                }) { classRoom ->
-                    ClassRoomCard(classRoom, onClassRoomClicked)
+                Text(
+                    text = "Bienvenue",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier.padding(it)
+                ) {
+                    items(classRooms.filter {
+                        it.name.contains(
+                            searchText.value.text,
+                            true
+                        )
+                    }) { classRoom ->
+                        ClassRoomCard(classRoom, onClassRoomClicked)
+                    }
                 }
+            }
+        },
+        bottomBar = {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+            ){
+                BottomMenu(
+                    onHomeClicked = { },
+                    onFavoriteClicked = {  },
+                    onSettingsClicked = {  }
+
+                )
             }
         }
     )
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ClassRoomCard(
     classRoom: ClassRoom,
@@ -74,28 +147,63 @@ fun ClassRoomCard(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(8.dp),
         shape = RoundedCornerShape(8.dp),
+        onClick = { onClassRoomClicked(classRoom) }
     ) {
-        Row(
-            modifier = Modifier.padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+        Box(
+            modifier = Modifier
+                .padding(5.dp)
+                .width(800.dp)
+                .height(170.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(
+                    GraphicUtils.getMainBrush(
+                        MaterialTheme.colorScheme.secondary,
+                        MaterialTheme.colorScheme.tertiary
+                    )
+                )
         ) {
-            classRoom.image?.let { painterResource(it) }?.let {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+                    .align(Alignment.TopCenter)
+                    .padding(vertical = 16.dp)
+            ) {
                 Image(
-                    painter = it,
-                    contentDescription = "Classroom Image"
+                    painter = painterResource(R.drawable.logoclass1),
+                    contentDescription = "Logo de la salle de classe",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f)
                 )
             }
             Column(
-                modifier = Modifier.padding(start = 16.dp)
+                modifier = Modifier
+                    .align(Alignment.BottomCenter),
+                horizontalAlignment = Alignment.CenterHorizontally
+
             ) {
-                Text(text = classRoom.name)
-                classRoom.description?.let { Text(text = it) }
-                Button(onClick = {onClassRoomClicked(classRoom)}) {
-                    Text("View Details")
-                }
+                Text(
+                    text = classRoom.name,
+                    color = Color.White,
+                    fontSize = 28.sp,
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontStyle = FontStyle.Normal)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
